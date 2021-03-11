5c1d01388a6eafa351b8a41b8a92eca984da6ef7
Matthew Stevenson <mattstev@google.com>
2021-03-11 08:39:34 -0800

    Expand run_tests.sh to set Bazel version.

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,38 @@
 
 # Fail on any error.
 set -e
+# Display commands being run.
+set -x
 
-bazel build ...
-bazel test --test_output=errors -- ...
+fail_with_debug_output() {
+  ls -l
+  df -h /
+  exit 1
+}
+
+run_tests() {
+  local -a TEST_FLAGS=( --strategy=TestRunner=standalone --test_output=all )
+  readonly TEST_FLAGS
+  (
+    time bazel build -- ... || fail_with_debug_output
+    time bazel test "${TEST_FLAGS[@]}" -- ... || fail_with_debug_output
+  )
+}
+
+main() {
+  if [[ -n "${KOKORO_ROOT}" ]]; then
+    use_bazel.sh $(cat .bazelversion)
+    # Install protoc into a temporary directory.
+    install_temp_protoc
+  fi
+
+  echo "using bazel binary: $(which bazel)"
+  bazel version
+
+  echo "using protoc: $(which protoc)"
+  protoc --version
+
+  run_tests
+}
+
+main "$@"
