16c6bfbccaedad139d76c998429251a64238357b
Matthew Stevenson <mattstev@google.com>
2021-03-11 19:04:39 -0800

    Add GCP Windows Kokoro config.'

---
+++ tools/internal_ci/run_tests.bat
@@ -0,0 +1,6 @@
+choco install bazel -y --version 4.0.0 --limit-output
+cd github/s2a-core
+set PATH=C:\tools\msys64\usr\bin;C:\Python27;%PATH%
+bazel test //...
+set BAZEL_EXITCODE=%errorlevel%
+exit /b %BAZEL_EXITCODE%
