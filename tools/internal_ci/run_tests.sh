6d5fa6a9a380ed5dd2c53d9f5a86b6f94b771efa
Matthew Stevenson <mattstev@google.com>
2021-04-22 15:37:54 -0700

    retrying

--- tools/internal_ci/run_tests.sh
+++ tools/internal_ci/run_tests.sh
@@ -28,7 +28,7 @@ run_tests_on_linux() {
 
 run_tests_on_macos() {
   # Was necessary to avoid a temporary Kokoro error.
-  # pip install --no-binary :all: psutil
+  #  pip install --no-binary :all: psutil
 
   # The OpenSSL library used in OpenSSL-specific tests is only configured to run
   # on Linux, so omit targets with the "openssl" tag.
