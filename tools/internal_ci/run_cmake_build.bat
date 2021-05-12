9d83846bb293f1460271948dd051a0d543debafe
Matthew Stevenson <mattstev@google.com>
2021-05-12 12:48:38 -0700

    Add value when defining the variable.

--- tools/internal_ci/run_cmake_build.bat
+++ tools/internal_ci/run_cmake_build.bat
@@ -16,4 +16,4 @@ cd github/s2a-core
 git submodule update --init --recursive
 echo "================================= Running cmake"
 cmake --version
-cmake . -DDEFINE_S2A_CORE_USE_NEW_UPB_APIS -DCMAKE_CXX_STANDARD=11 || goto :error
+cmake . -DDEFINE_S2A_CORE_USE_NEW_UPB_APIS=1 -DCMAKE_CXX_STANDARD=11 || goto :error
