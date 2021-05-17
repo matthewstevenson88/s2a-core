be5578547609319a44832f301e4f953bc919788c
Matthew Stevenson <mattstev@google.com>
2021-05-17 11:47:11 -0700

    print CMakeFiles directory on Windows

--- tools/internal_ci/run_cmake_build.bat
+++ tools/internal_ci/run_cmake_build.bat
@@ -18,3 +18,4 @@ echo "================================= Running cmake"
 cmake --version
 cmake . -DDEFINE_S2A_CORE_USE_NEW_UPB_APIS=1 -DCMAKE_CXX_STANDARD=11 || goto :error
 ls -l
+ls -l CMakeFiles
