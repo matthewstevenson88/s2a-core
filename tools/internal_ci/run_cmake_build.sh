ce994f068b4077dc81628c63e19f05c0f8adba13
Matthew Stevenson <mattstev@google.com>
2021-06-01 16:06:40 -0700

    trying once again

--- tools/internal_ci/run_cmake_build.sh
+++ tools/internal_ci/run_cmake_build.sh
@@ -32,8 +32,17 @@ case "${PLATFORM}" in
 
     openssl version
     openssl version -a
-    sudo apt-get update
-    sudo apt-get upgrade openssl
+    #sudo apt-get update
+    #sudo apt-get upgrade openssl
+
+    wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz
+    tar -zxf openssl-1.1.1g.tar.gz && cd openssl-1.1.1g
+    ./config
+    make
+    sudo mv /usr/bin/openssl ~/tmp
+    sudo make install
+    sudo ln -s /usr/local/bin/openssl /usr/bin/openssl
+    sudo ldconfig
 
     openssl version
     openssl version -a
