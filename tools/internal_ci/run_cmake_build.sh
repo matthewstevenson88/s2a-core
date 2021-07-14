784dd39a3e8f1c3910c09449477e54e9b8c06a6a
Matthew Stevenson <mattstev@google.com>
2021-07-14 12:29:13 -0700

    remove some more dirs

--- tools/internal_ci/run_cmake_build.sh
+++ tools/internal_ci/run_cmake_build.sh
@@ -45,8 +45,10 @@ case "${PLATFORM}" in
     rm '/usr/local/bin/protoc'
     rm '/usr/local/bin/pydoc3'
     rm '/usr/local/bin/python3'
+    rm '/usr/local/bin/python3-config'
     rm '/usr/local/include/google/protobuf/any.proto'
     rm '/usr/local/include/google/protobuf/api.proto'
+    rm '/usr/local/include/google/protobuf/compiler/plugin.proto'
 
     brew update
     brew unlink ilmbase
