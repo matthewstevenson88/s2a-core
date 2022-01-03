cc72ddc446eb89674c30cd5237d2d4a6d4a06908
Matthew Stevenson <mattstev@google.com>
2022-01-03 10:27:04 -0800

    Attempt to re-install only command line tools, not Xcode

--- tools/internal_ci/run_cmake_build.sh
+++ tools/internal_ci/run_cmake_build.sh
@@ -52,7 +52,19 @@ case "${PLATFORM}" in
 
     # Clean re-install of XCode's command line tools.
     sudo rm -Rf '/Library/Developer/CommandLineTools'
-    sudo xcode-select --install
+    xcode-select -p &> /dev/null
+    if [ $? -ne 0 ]; then
+      echo "Xcode CLI tools not found. Installing them..."
+      touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+      PROD=$(softwareupdate -l |
+        grep "\*.*Command Line" |
+        head -n 1 | awk -F"*" '{print $2}' |
+        sed -e 's/^ *//' |
+        tr -d '\n')
+      softwareupdate -i "$PROD" -v;
+    else
+      echo "Xcode CLI tools OK"
+fi
 
     # Update Homebrew and unlink ilmbase, which will be re-linked when openssl
     # is installed.
